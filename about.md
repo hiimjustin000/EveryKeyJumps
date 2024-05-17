2c7fdd15fe87f0a9a5af87fdd69cde3810e5c6ff
Justin Pridgen <justincpridgen672@gmail.com>
2024-05-17 00:26:03 -0400

    v1.0.0

--- about.md
+++ about.md
@@ -1,7 +1,7 @@
 # Every Key Jumps
-A mod that allows you to jump with almost every key on your keyboard on classic mode.
+A mod that allows you to jump with almost every key on your keyboard.
 - NOTE: This mod is not compatible with the "Click Between Frames" mod by syzzi.
 
 # Features
-- Jump with almost every key on your keyboard in classic mode. Platformer mode is not supported.
+- Jump with almost every key on your keyboard.
 - It is not required to disable the mod and then restart the game. In settings, you can disable the "Enable Every Key Jumps" option to disable the mod.
--- src/main.cpp
+++ src/main.cpp
@@ -56,9 +56,12 @@ class $modify(EKJKeyboardDispatcher, CCKeyboardDispatcher) {
     bool dispatchKeyboardMSG(enumKeyCodes key, bool down, bool repeat) {
         auto gameManager = GameManager::sharedState();
         auto player1 = std::find(p1Keys.begin(), p1Keys.end(), key) != p1Keys.end();
-        if (gameManager->m_playLayer && !gameManager->m_playLayer->m_levelSettings->m_platformerMode && (player1 || std::find(p2Keys.begin(), p2Keys.end(), key) != p2Keys.end())) {
-            if (!gameManager->m_playLayer->m_isPracticeMode || (key != enumKeyCodes::KEY_X && key != enumKeyCodes::KEY_Z))
-                gameManager->m_playLayer->handleButton(down, 1, player1);
+        if (gameManager->m_playLayer && (player1 || std::find(p2Keys.begin(), p2Keys.end(), key) != p2Keys.end())) {
+            if (!gameManager->m_playLayer->m_levelSettings->m_platformerMode ||
+                (key != enumKeyCodes::KEY_W && key != enumKeyCodes::KEY_A && key != enumKeyCodes::KEY_S && key != enumKeyCodes::KEY_D)) {
+                if (!gameManager->m_playLayer->m_isPracticeMode || (key != enumKeyCodes::KEY_X && key != enumKeyCodes::KEY_Z))
+                    gameManager->m_playLayer->handleButton(down, 1, player1);
+            }
         }
         return CCKeyboardDispatcher::dispatchKeyboardMSG(key, down, repeat);
     }
