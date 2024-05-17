f0c5431937f35a987fe1c59af4ad24cc37989736
Justin Pridgen <justincpridgen672@gmail.com>
2024-05-17 02:49:29 -0400

    v1.0.0

--- src/main.cpp
+++ src/main.cpp
@@ -24,8 +24,7 @@ std::vector<enumKeyCodes> p1Keys = {
     enumKeyCodes::KEY_X,
     enumKeyCodes::KEY_C,
     enumKeyCodes::KEY_V,
-    enumKeyCodes::KEY_B,
-    enumKeyCodes::KEY_LeftShift
+    enumKeyCodes::KEY_B
 };
 
 std::vector<enumKeyCodes> p2Keys = {
@@ -45,7 +44,6 @@ std::vector<enumKeyCodes> p2Keys = {
     enumKeyCodes::KEY_L,
     enumKeyCodes::KEY_N,
     enumKeyCodes::KEY_M,
-    enumKeyCodes::KEY_RightShift,
     enumKeyCodes::KEY_OEMComma,
     enumKeyCodes::KEY_OEMPeriod,
     enumKeyCodes::KEY_Enter
@@ -54,13 +52,15 @@ std::vector<enumKeyCodes> p2Keys = {
 #include <Geode/modify/CCKeyboardDispatcher.hpp>
 class $modify(EKJKeyboardDispatcher, CCKeyboardDispatcher) {
     bool dispatchKeyboardMSG(enumKeyCodes key, bool down, bool repeat) {
-        auto gameManager = GameManager::sharedState();
-        auto player1 = std::find(p1Keys.begin(), p1Keys.end(), key) != p1Keys.end();
-        if (gameManager->m_playLayer && (player1 || std::find(p2Keys.begin(), p2Keys.end(), key) != p2Keys.end())) {
-            if (!gameManager->m_playLayer->m_levelSettings->m_platformerMode ||
-                (key != enumKeyCodes::KEY_W && key != enumKeyCodes::KEY_A && key != enumKeyCodes::KEY_S && key != enumKeyCodes::KEY_D)) {
-                if (!gameManager->m_playLayer->m_isPracticeMode || (key != enumKeyCodes::KEY_X && key != enumKeyCodes::KEY_Z))
-                    gameManager->m_playLayer->handleButton(down, 1, player1);
+        if (Mod::get()->getSettingValue<bool>("ekj-enabled")) {
+            auto gameManager = GameManager::sharedState();
+            auto player1 = std::find(p1Keys.begin(), p1Keys.end(), key) != p1Keys.end();
+            if (gameManager->m_playLayer && (player1 || std::find(p2Keys.begin(), p2Keys.end(), key) != p2Keys.end())) {
+                if (!gameManager->m_playLayer->m_levelSettings->m_platformerMode ||
+                    (key != enumKeyCodes::KEY_W && key != enumKeyCodes::KEY_A && key != enumKeyCodes::KEY_S && key != enumKeyCodes::KEY_D)) {
+                    if (!gameManager->m_playLayer->m_isPracticeMode || (key != enumKeyCodes::KEY_X && key != enumKeyCodes::KEY_Z))
+                        gameManager->m_playLayer->handleButton(down, 1, player1);
+                }
             }
         }
         return CCKeyboardDispatcher::dispatchKeyboardMSG(key, down, repeat);
