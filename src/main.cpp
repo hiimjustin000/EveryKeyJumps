87029d74231a025a5f048f9f6191540ed0a84e9a
Justin Pridgen <justincpridgen672@gmail.com>
2024-05-27 12:49:21 -0400

    v1.0.1

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,6 @@
 
 using namespace geode::prelude;
 
-//#define MBO(type, clazz, offset) *reinterpret_cast<type*>(reinterpret_cast<uintptr_t>(clazz) + offset)
-
 std::vector<enumKeyCodes> p1Keys = {
     enumKeyCodes::KEY_One,
     enumKeyCodes::KEY_Two,
@@ -66,61 +64,3 @@ class $modify(EKJKeyboardDispatcher, CCKeyboardDispatcher) {
         return CCKeyboardDispatcher::dispatchKeyboardMSG(key, down, repeat);
     }
 };
-
-/*#include <Geode/modify/GJBaseGameLayer.hpp>
-class $modify(EKJBaseGameLayer, GJBaseGameLayer) {
-    void handleButton(bool down, int button, bool player1) {
-        log::info("Run the function ok");
-        if (button == 100) {
-            log::info("Running this stupid override");
-            auto twoPlayer = m_levelSettings->m_twoPlayerMode;
-            auto isPlayer1 = player1;
-            if (!MBO(bool, this, 0x29c6) && !MBO(bool, this, 0x2a44)) {
-                if (GameManager::sharedState()->getGameVariable("0010")) isPlayer1 = !player1;
-            }
-
-            auto hasActiveDualTouch = false;
-            for (char* p = MBO(char*, this->m_effectManager, 0x160); p != MBO(char*, this->m_effectManager, 0x164); p += 0x2c) {
-                if (MBO(bool, p, 0x1c) && *p == 0) {
-                    hasActiveDualTouch = true;
-                    break;
-                }
-            }
-
-            if (down) MBO(bool, this, 0x29a4) = hasActiveDualTouch;
-            else if (!MBO(bool, this, 0x29a4)) hasActiveDualTouch = false;
-
-            auto randomBool = false;
-            if (!hasActiveDualTouch) {
-                if (!isPlayer1) hasActiveDualTouch = twoPlayer;
-
-                if (isPlayer1 || (!isPlayer1 && !twoPlayer)) {
-                    randomBool = hasActiveDualTouch;
-                    if (down) m_player1->pushButton(PlayerButton::Jump);
-                    else m_player1->releaseButton(PlayerButton::Jump);
-                }
-                
-                auto otherBool = false;
-                if ((!MBO(bool, this, 0x29a4) && m_gameState.m_isDualMode) && (!isPlayer1 || !twoPlayer)) {
-                    randomBool = twoPlayer;
-                    otherBool = true;
-                    if (down) m_player2->pushButton(PlayerButton::Jump);
-                    else m_player2->releaseButton(PlayerButton::Jump);
-                }
-
-                if (randomBool) {
-                    hasActiveDualTouch = twoPlayer || otherBool;
-                    gameEventTriggered(down ? GJGameEvent::JumpPush : GJGameEvent::JumpRelease, 0, hasActiveDualTouch ? 2 : 1);
-                }
-            }
-
-            if (!m_player1->m_isDead) m_effectManager->playerButton(down, isPlayer1);
-            if (!MBO(bool, this, 0x2a44)) recordAction(1, down, twoPlayer && isPlayer1);
-            if (down) {
-                MBO(int, this, 0x29a8) += 1;
-                MBO(int, this, 0x29b0) = 1;
-            }
-        }
-        else GJBaseGameLayer::handleButton(down, button, player1);
-    }
-};*/
