d2c95bbcdd166268acc01309d8a66480b6fd9894
Justin Pridgen <justincpridgen672@gmail.com>
2024-05-28 16:22:09 -0400

    v1.1.0

--- src/main.cpp
+++ src/main.cpp
@@ -3,64 +3,37 @@
 using namespace geode::prelude;
 
 std::vector<enumKeyCodes> p1Keys = {
-    enumKeyCodes::KEY_One,
-    enumKeyCodes::KEY_Two,
-    enumKeyCodes::KEY_Three,
-    enumKeyCodes::KEY_Four,
-    enumKeyCodes::KEY_Five,
-    enumKeyCodes::KEY_Q,
-    enumKeyCodes::KEY_W,
-    enumKeyCodes::KEY_E,
-    enumKeyCodes::KEY_R,
-    enumKeyCodes::KEY_T,
-    enumKeyCodes::KEY_A,
-    enumKeyCodes::KEY_S,
-    enumKeyCodes::KEY_D,
-    enumKeyCodes::KEY_F,
-    enumKeyCodes::KEY_G,
-    enumKeyCodes::KEY_Z,
-    enumKeyCodes::KEY_X,
-    enumKeyCodes::KEY_C,
-    enumKeyCodes::KEY_V,
-    enumKeyCodes::KEY_B
+    KEY_One, KEY_Two, KEY_Three, KEY_Four,
+    KEY_Five, KEY_Q, KEY_W, KEY_E,
+    KEY_R, KEY_T, KEY_A, KEY_S,
+    KEY_D, KEY_F, KEY_G, KEY_Z,
+    KEY_X, KEY_C, KEY_V, KEY_B
 };
 
 std::vector<enumKeyCodes> p2Keys = {
-    enumKeyCodes::KEY_Six,
-    enumKeyCodes::KEY_Seven,
-    enumKeyCodes::KEY_Eight,
-    enumKeyCodes::KEY_Nine,
-    enumKeyCodes::KEY_Zero,
-    enumKeyCodes::KEY_Y,
-    enumKeyCodes::KEY_U,
-    enumKeyCodes::KEY_I,
-    enumKeyCodes::KEY_O,
-    enumKeyCodes::KEY_P,
-    enumKeyCodes::KEY_H,
-    enumKeyCodes::KEY_J,
-    enumKeyCodes::KEY_K,
-    enumKeyCodes::KEY_L,
-    enumKeyCodes::KEY_N,
-    enumKeyCodes::KEY_M,
-    enumKeyCodes::KEY_OEMComma,
-    enumKeyCodes::KEY_OEMPeriod,
-    enumKeyCodes::KEY_Enter
+    KEY_Six, KEY_Seven, KEY_Eight, KEY_Nine,
+    KEY_Zero, KEY_Y, KEY_U, KEY_I,
+    KEY_O, KEY_P, KEY_H, KEY_J,
+    KEY_K, KEY_L, KEY_N, KEY_M,
+    KEY_OEMComma, KEY_OEMPeriod, KEY_Enter
 };
 
 #include <Geode/modify/CCKeyboardDispatcher.hpp>
 class $modify(EKJKeyboardDispatcher, CCKeyboardDispatcher) {
     bool dispatchKeyboardMSG(enumKeyCodes key, bool down, bool repeat) {
-        if (Mod::get()->getSettingValue<bool>("ekj-enabled") && !repeat) {
-            auto gameManager = GameManager::sharedState();
+        if (!repeat && Mod::get()->getSettingValue<bool>("ekj-enabled")) {
+            auto gameLayer = GameManager::sharedState()->m_gameLayer;
+            if (!gameLayer) goto LABEL_RETURN;
+
             auto player1 = std::find(p1Keys.begin(), p1Keys.end(), key) != p1Keys.end();
-            if (gameManager->m_playLayer && (player1 || std::find(p2Keys.begin(), p2Keys.end(), key) != p2Keys.end())) {
-                if (!gameManager->m_playLayer->m_levelSettings->m_platformerMode ||
-                    (key != enumKeyCodes::KEY_W && key != enumKeyCodes::KEY_A && key != enumKeyCodes::KEY_S && key != enumKeyCodes::KEY_D)) {
-                    if (!gameManager->m_playLayer->m_isPracticeMode || (key != enumKeyCodes::KEY_X && key != enumKeyCodes::KEY_Z))
-                        gameManager->m_playLayer->handleButton(down, 1, player1);
-                }
-            }
+            if (!player1 && std::find(p2Keys.begin(), p2Keys.end(), key) == p2Keys.end()) goto LABEL_RETURN;
+
+            if (gameLayer->m_levelSettings->m_platformerMode && (key == KEY_A || key == KEY_D)) goto LABEL_RETURN;
+            if (gameLayer->m_isPracticeMode && (key == KEY_W || key == KEY_Z)) goto LABEL_RETURN;
+
+            gameLayer->handleButton(down, 1, player1);
         }
+        LABEL_RETURN:
         return CCKeyboardDispatcher::dispatchKeyboardMSG(key, down, repeat);
     }
 };
